6f48b1724e72610334d029af4ed8e3a34210e2a9
Davi Nasiasene Amorim <davi.ted@hotmail.com>
2023-03-15 20:04:47 -0300

    A Band Name generator.

---
+++ codes/day01.py
@@ -0,0 +1,9 @@
+#Band name generator!!
+
+
+print('Welcome to the band name generator!!\n')
+
+city = input('Which city did you grew up in?\n')
+pet = input('What is the name of your pet?\n')
+
+print('The name of your band could be', city, pet) 
