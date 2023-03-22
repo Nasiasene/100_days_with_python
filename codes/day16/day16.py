242ed748b4cfbadaa4da7d523e3440478655d4b6
Davi Nasiasene Amorim <davi.ted@hotmail.com>
2023-03-21 23:14:11 -0300

    A CoffeeMachine2.0 with OOP.

--- codes/day16/day16.py
+++ codes/day16/day16.py
@@ -1,4 +1,28 @@
-import menu
-import money_machine
-import coffee_maker
+from menu import Menu
+from money_machine import MoneyMachine
+from coffee_maker import CoffeeMaker
 
+menu = Menu()
+machine = CoffeeMaker()
+money_machine = MoneyMachine()
+
+
+on_off = True
+while on_off:
+    order_coffee = input(f"\nWhat would you like?{menu.get_items()}: ")
+    if order_coffee == 'off':
+        on_off = False
+        break
+    elif order_coffee == 'report':
+        machine.report()
+        money_machine.report()
+        continue
+    else:
+        if menu.find_drink(order_coffee):
+            order_coffee = menu.find_drink(order_coffee)
+        else:
+            continue
+        if machine.is_resource_sufficient(order_coffee):
+            cost_coffee = order_coffee.cost
+            if money_machine.make_payment(cost_coffee):
+                machine.make_coffee(order_coffee)
