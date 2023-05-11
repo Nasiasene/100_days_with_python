b65d80ae6268f608d0e207621d059401a45700f9
Davi Nasiasene Amorim <davi.ted@hotmail.com>
2023-05-11 09:15:07 -0300

    NATO Alphabet project

--- codes/day26/main.py
+++ codes/day26/main.py
@@ -7,6 +7,14 @@ df = pd.read_csv("NATO_alphabet.csv")
 new_dict = {row['letter']:row['code'] for (index, row) in df.iterrows()}
 
 
-word = input("Insert a word: ").upper()
-new_list = [new_dict[n] for n in word]
-print(new_list)
+def generate_word():
+    word = input("Insert a word: ").upper()
+    try:
+        new_list = [new_dict[n] for n in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_word()
+    else:
+        print(new_list)
+
+generate_word()
