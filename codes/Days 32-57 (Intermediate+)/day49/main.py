2a1d6e59517873712f607d0e8b45ac1e347dc8d5
Davi Nasiasene Amorim <davi.ted@hotmail.com>
2023-07-11 17:11:22 -0300

    Automating Jobs Applications on linkedin

--- codes/Days 32-57 (Intermediate+)/day49/main.py
+++ codes/Days 32-57 (Intermediate+)/day49/main.py
@@ -5,8 +5,9 @@ from selenium.webdriver.common.keys import Keys
 import time
 import os
 
-#EMAIL=os.environ["EMAIL"]
-#PASSWORD=os.environ["PASSWORD"]
+
+EMAIL=os.environ["EMAIL"]
+PASSWORD=os.environ["PASSWORD"]
 
 service = Service("C:\Developer\chromedriver.exe")
 driver = webdriver.Chrome(service=service)
@@ -15,8 +16,26 @@ driver.get("https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491&keyw
 
 driver.maximize_window()
 
-time.sleep(5)
-
+#Openning linkedin and sign up
+time.sleep(1)
 button = driver.find_element(By.XPATH, "/html/body/div[3]/a[1]")
-print(button.text)
+button.click()
+
+email_answer = driver.find_element(By.ID, 'username')
+email_answer.send_keys(EMAIL)
+password_answer =driver.find_element(By.ID, 'password')
+password_answer.send_keys(PASSWORD)
+check_button = driver.find_element(By.XPATH, '//*[@id="organic-div"]/form/div[3]/button')
+check_button.click()
+time.sleep(3)
+
+#Saving
+page = driver.find_elements(By.CSS_SELECTOR, "ul.scaffold-layout__list-container li")
+for i in page:
+    i.click()
+    time.sleep(0.5)
+    save_button = driver.find_element(By.CSS_SELECTOR, '.display-flex .jobs-save-button')
+    save_button.click()
+
+time.sleep(10)
 driver.quit()
