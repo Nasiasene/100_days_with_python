1d7435151b2e5b49c65cfc67e23f63dc4a867caa
Davi Nasiasene Amorim <davi.ted@hotmail.com>
2023-08-08 16:46:06 -0300

    firts steps with flask

--- codes/Days 32-57 (Intermediate+)/day54_55/hello.py
+++ codes/Days 32-57 (Intermediate+)/day54_55/hello.py
@@ -9,12 +9,12 @@ def hello_world():
 #>set FLASK_APP=hello.py
 #>flask run
 
-@app.route('/test')
-def test():
-    return "testing"
+@app.route('/<test>/<int:num>') #<> create an variable.
+def test(test, num):
+    return f'<h1>{test} - {num}<h1>' #flask can return html code
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
 #will run the code with out using the prompt.
 #when running the main file directly, the value of the __name__ variable is set to "__main__".
